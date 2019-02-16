6bdce6abaf9e1071f55fec65aae559d986a8dc00
Francisco Márquez Chaves <francisco.marquez@dominion-global.com>
2019-02-16 11:43:12 +0100

    json Parses at beginning

---
+++ IdealistaTest/Infrastructure/Entities/Ad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdealistaTest.Infrastructure.Entities
+{
+    public class Ad
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int HouseSize { get; set; }
+        public int GardenSize { get; set; }
+        public List<int> Pictures { get; set; }
+    }
+}
--- IdealistaTest/Infrastructure/FakeDatabase.cs
+++ IdealistaTest/Infrastructure/FakeDatabase.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using IdealistaTest.Infrastructure.Entities;
+using Newtonsoft.Json;
 
 namespace IdealistaTest.Infrastructure
 {
@@ -30,7 +33,12 @@ namespace IdealistaTest.Infrastructure
 
         public  void InitializeDatabase()
         {
-            
+            string adJsonFilename =
+                System.Web.Hosting.HostingEnvironment.MapPath(@"~/Infrastructure/jsonPopulateFiles/ads.json");
+            var ads = JsonConvert.DeserializeObject<List<Ad>>(File.ReadAllText(adJsonFilename));
+            string pictureJsonFilename =
+                System.Web.Hosting.HostingEnvironment.MapPath(@"~/Infrastructure/jsonPopulateFiles/pictures.json");
+            var pictures = JsonConvert.DeserializeObject<List<Picture>>(File.ReadAllText(pictureJsonFilename));
         }
     }
 }
